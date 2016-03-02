d02b7e9483ec550dbb855b567096c22ae37a04bf
Santosh Kumar Desai <santoshdesai12@hotmail.com>
2016-03-02 14:33:50 +0530

    Minor Update
    Corrected some issues with input

--- Vinata/src/vinata/FXMLDocumentController.java
+++ Vinata/src/vinata/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package vinata;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -53,7 +54,15 @@ public class FXMLDocumentController
         if(!(model.isOperator(init1)||model.isParen(init1)||model.isExtra(init1)))
         {
             //System.out.println(init2+"_"+init1);
-            if(!arrayList.isEmpty()&&!model.isOperator(arrayList.get(arrayList.size()-1))&&!model.isParen(arrayList.get(arrayList.size()-1))&&!model.isExtra(arrayList.get(arrayList.size()-1)))
+            if(!arrayList.isEmpty())
+            {
+            if(input.getText().endsWith("e")||input.getText().endsWith("π"))
+            {
+                input.setText(input.getText()+"*");
+                arrayList.add("*");
+                arrayList.add(init1);
+            }
+            else if(!model.isOperator(arrayList.get(arrayList.size()-1))&&!model.isParen(arrayList.get(arrayList.size()-1))&&!model.isExtra(arrayList.get(arrayList.size()-1)))
             {
                 if(dot&&init1.equals("."))
                     init1="";
@@ -61,12 +70,22 @@ public class FXMLDocumentController
                     dot=true;
                 init2 = arrayList.get((arrayList.size()-1)) + init1;
                 arrayList.remove((arrayList.size()-1));
+                arrayList.add(init2);
+            } 
+            else if(")".equals(arrayList.get(arrayList.size()-1)))
+            {
+                arrayList.add("*");
+                arrayList.add(init1);
+            }
             }
             else
+            {
                 init2 = init2 + init1;
-            arrayList.add(init2);
+                arrayList.add(init2);
+            }
             init2="";
             input.setText(input.getText()+init1);
+            
         }
         else if(model.isParen(init1))
         {
@@ -76,13 +95,17 @@ public class FXMLDocumentController
         }
         else if(model.isOperator(init1))
         {
-            if(model.isOperator(arrayList.get((arrayList.size()-1))))
-                    init1="";
             if(arrayList.isEmpty())
             {
                 input.setText("0");
                 arrayList.add(Double.toString(0));
             }
+            else if(arrayList.get((arrayList.size()-1)).equals("("))
+            {
+                arrayList.add("0");
+            }
+            else if(model.isOperator(arrayList.get((arrayList.size()-1))))
+                    init1="";
             if(!init1.equals(""))
                 arrayList.add(init1);
             dot = false;
@@ -93,13 +116,38 @@ public class FXMLDocumentController
         {
             if(init1.contentEquals("e"))
             {
+                if(!arrayList.isEmpty())
+                {
+                if("(".equals(arrayList.get(arrayList.size()-1)))
+                {    
+                    arrayList.add("0");
+                    arrayList.add("+");
+                    init1="0+"+init1;
+                }
+                else if(")".equals(arrayList.get(arrayList.size()-1)))
+                {
+                    arrayList.add("*");
+                    init1="*"+init1;
+                }
+                else if(!model.isOperator(arrayList.get(arrayList.size()-1))&&!model.isParen(arrayList.get(arrayList.size()-1)))
+                {    
+                    arrayList.add("*");
+                    init1="*"+init1;
+                }
+                }
                 arrayList.add(Double.toString(Math.E));
                 input.setText(input.getText()+init1);
             }
             else if(init1.contentEquals("r"))
             {
+                if(!arrayList.isEmpty())
+                {
                 boolean b=false;
                 String y = arrayList.get((arrayList.size()-1));
+                if("e".equals(y))
+                    y=Double.toString(Math.E);
+                if("π".equals(y))
+                    y=Double.toString(Math.PI);
                 double id = 0,d = 0;
                 if(!(model.isOperator(init1)||model.isParen(init1)))
                 {
@@ -121,31 +169,64 @@ public class FXMLDocumentController
                 input.setText(s);//replace(y,Double.toString(id)));
                 if(b)
                     input.setText("Div By Zero Error");
+                }
             }
             else if(init1.contentEquals("√"))
             {
+                if(!arrayList.isEmpty())
+                {
                 String y = arrayList.get((arrayList.size()-1));
+                if("e".equals(y))
+                    y=Double.toString(Math.E);
+                if("π".equals(y))
+                    y=Double.toString(Math.PI);
                 double d = Double.parseDouble(y);
                 double sqrtd= Math.sqrt(d);
                 arrayList.remove((arrayList.size()-1));
                 arrayList.add(Double.toString(sqrtd));
                 init1="";
+                }
             }
             else if(init1.contentEquals("π"))
             {
+                if(!arrayList.isEmpty())
+                {
+                if("(".equals(arrayList.get(arrayList.size()-1)))
+                {    
+                    arrayList.add("0");
+                    arrayList.add("+");
+                    init1="0+"+init1;
+                }
+                else if(")".equals(arrayList.get(arrayList.size()-1)))
+                {
+                    arrayList.add("*");
+                    init1="*"+init1;
+                }
+                else if(!model.isOperator(arrayList.get(arrayList.size()-1))&&!model.isParen(arrayList.get(arrayList.size()-1)))
+                {    
+                    arrayList.add("*");
+                    init1="*"+init1;
+                }
+                }
                 arrayList.add(Double.toString(Math.PI));
                 input.setText(input.getText()+init1);
             }
             else if(init1.contentEquals("←"))
             {
+                //for (String i : arrayList) {
+                  //  System.out.println(i);
+                //}
+                if(!arrayList.isEmpty())
+                {
                 String y = arrayList.get((arrayList.size()-1));
                 y = model.removeLastChar(y);
                 arrayList.remove((arrayList.size()-1));
                 arrayList.add(y);
-                if(y=="")
+                if("".equals(y))
                     arrayList.remove((arrayList.size()-1));
                 input.setText(model.removeLastChar(input.getText()));
                 init1="";
+                }
             }
         }
     }
@@ -166,10 +247,13 @@ public class FXMLDocumentController
         {
             result.setText("Div By Zero error");
         }
+        catch(EmptyStackException e)
+        {
+            result.setText("Incomplete expression / Parenthesis error");
+        }
         catch(Exception e)
         {
-            e.printStackTrace();
-            //result.setText("Parenthesis error");
+            result.setText(e.getClass().getSimpleName());
         }
     }
     public void clear(ActionEvent event)
